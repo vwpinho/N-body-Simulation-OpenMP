e8868192027e10668c4e52e18049143c73e0bb05
vwpinho <vwdpinho@inf.ufpel.edu.br>
2020-10-26 13:56:23 -0300

    Definicoes iniciais

---
+++ main.cpp
@@ -0,0 +1,46 @@
+/*
+    Alunos: Vitor Wachholz de Pinho e Igor Rosler
+    Disciplina: IPPD
+    Uso: 
+        Compilacao: 
+            g++ -fopenmp main.cpp -lpthread -o out 
+            ./out
+        
+Implementação em OpenMP da simulação de n-corpos (n-body simulation). O material a ser entregue deve incluir:
+
+-Um arquivo README.txt que informe o conteúdo do diretório e 
+de as dicas necessárias para compilação e execução
+-Arquivos fontes e makefile para gerar o código executável
+-Arquivos adicionais (como base de dados) utilizadas como entrada do problema
+-Importante: o makefile deve ter uma opção para executar o programa variando o número 
+de threads e permitindo coletar o tempo de execução do programa em cada caso.
+-Uma análise de desempenho da implementação realizada, na forma de gráficos em um PDF. 
+-Esta análise de desempenho deve considerar a execução com diferente número de threads 
+e diferentes tamanhos do arquivo de entrada, se possível, variar também os computadores 
+utilizados
+*/
+
+// 1. Criar classe para os corpos
+// 2. Fazer as funções necessarias para os calculos
+// 3. Testar os calculos
+// 4. Adicionar a multiprogramação (OpenMP)
+// 5. Ler os parametros de um arquivo
+// 6. Criar make file
+// 7. Coletar dados para gerar os graficos
+// 8. Fazer uma analise do desempenho
+// 9. Redigir um README bem explicado
+
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+
+int main(){
+    
+    
+    return 0;
+}
+
